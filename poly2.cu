8c482fec907642e1df80bcb7356576211c0b88b4
canesche <michael.canesche@gmail.com>
2019-09-16 15:26:45 -0300

    a

--- poly2.cu
+++ poly2.cu
@@ -79,8 +79,8 @@ __global__ void poli4(float* poli, const int N) {
 }
 */
 int main() {
-    //int nElem = 1 << 26;
-    int nElem = 1 << 4;
+    int nElem = 1 << 26;
+    //int nElem = 1 << 4;
 
     size_t nBytes = nElem * sizeof(float);
 
